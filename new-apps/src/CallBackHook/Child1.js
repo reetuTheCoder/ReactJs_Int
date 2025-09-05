aaacd00931704c75dbfc255bc9a293477dba7d81
reetuTheCoder <codewithreetu@gmail.com>
2025-09-05 15:16:40 +0530

    now Child1 component will not re render when Count will update

--- new-apps/src/CallBackHook/Child1.js
+++ new-apps/src/CallBackHook/Child1.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from "react";
 
-const Child1 = () => {
-     console.log("i am Child 1");
-  return (
-    <div>Child1</div>
-  )
-}
+const Child1 = React.memo(() => {
+  console.log("I am Child 1");
+  return <div>Child1</div>;
+});
 
-export default Child1
+export default Child1;
