dfe0a7df783fac8667a990c4b52b076f0ba38c91
reetuTheCoder <codewithreetu@gmail.com>
2025-09-07 12:55:40 +0530

    clean up file

--- solve-qst/src/App.js
+++ solve-qst/src/App.js
@@ -1,29 +1,11 @@
 import React from "react";
 import "./App.css";
-import SaveLSInputData from "./Componnet/SaveLSInputData";
-import MainProgressBar from "./Componnet/ProgressBar";
-import SelectAllListItems from "./Componnet/SelectAllListItems";
-import HoverCount from "./Componnet/HoverCount";
-import InputMsgEnter from "./Componnet/InputMsgEnter";
-import BuutonActionOnClick from "./Componnet/BuutonActionOnClick";
-import TemperatureConverter from "./Componnet/TemperatureConverter";
+
 
 function App() {
   return (
     <div className="App">
-      <SaveLSInputData />
-      <br /> <br /> <br />
-      <MainProgressBar />
-      <br /> <br /> <br />
-      <SelectAllListItems />
-      <br /> <br /> <br />
-      <HoverCount />
-      <br /> <br /> <br />
-      <InputMsgEnter />
-      <br /> <br /> <br />
-      <BuutonActionOnClick />
-       <br /> <br /> <br />
-      <TemperatureConverter/>
+   
     </div>
   );
 }
