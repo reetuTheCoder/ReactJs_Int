32a6697480263cf965145a55a8eef1a8ad757526
reetuTheCoder <codewithreetu@gmail.com>
2025-09-07 17:58:00 +0530

    just comment out the prevoius

--- solve-qst/src/useTheme.js
+++ solve-qst/src/useTheme.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 
-// const THEME_KEY = "app-theme";
+const THEME_KEY = "app-theme";
 
-// const getInitialTheme = () => {
-//   if (typeof window !== "undefined") {
-//      return localStorage.getItem(THEME_KEY) || "light";
-//     return "light";
-//   }
-//   return "light";
-// };
+const getInitialTheme = () => {
+  if (typeof window !== "undefined") {
+     return localStorage.getItem(THEME_KEY) || "light";
+    return "light";
+  }
+  return "light";
+};
 
 const useTheme = () => {
+  const [theme, setTheme] = useState(getInitialTheme);
 //   const [theme, setTheme] = useState(getInitialTheme);
-  const [theme, setTheme] = useState("light");
 
 
-//   useEffect(() => {
-//     localStorage.setItem(THEME_KEY, theme);
-//     document.documentElement.setAttribute("data-theme", theme);
-//   }, [theme]);
+  useEffect(() => {
+    localStorage.setItem(THEME_KEY, theme);
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
