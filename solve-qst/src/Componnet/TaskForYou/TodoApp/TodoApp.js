d81bc5bfc706d9416e111ec6e7c84053e34ca1b2
reetuTheCoder <codewithreetu@gmail.com>
2025-09-10 13:37:09 +0530

    add completed task option

--- solve-qst/src/Componnet/TaskForYou/TodoApp/TodoApp.js
+++ solve-qst/src/Componnet/TaskForYou/TodoApp/TodoApp.js
@@ -9,6 +9,7 @@ const TodoApp = () => {
     const newTask = {
       id: Date.now(),
       text: inputVal,
+      completed: false,
     };
     setTodos([...todos, newTask]);
     setInputVal("");
@@ -17,6 +18,22 @@ const TodoApp = () => {
   const handleDelete = (id) => {
     console.log("you Clicked delete", id);
     setTodos(todos.filter((task) => task.id !== id));
+  };
+
+  const handleAllDelete = () => {
+    setTodos([]);
+  };
+
+  const handleTaskCompleted = (id) => {
+    setTodos(
+      todos.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const completedCount = () => {
+    return todos.filter((task) => task.completed).length;
   };
   return (
     <div>
@@ -34,13 +51,40 @@ const TodoApp = () => {
       <div>
         <div>
           <ul>
-            {todos.map((task) => (
-              <li key={task.id}>
-                <span>{task.text}</span>
-                <button onClick={()=> handleDelete(task.id)}>Delete</button>
-              </li>
-            ))}
+            <div>
+              {todos.map((task) => (
+                <li key={task.id} onClick={() => handleTaskCompleted(task.id)}>
+                  <span
+                    style={{
+                      textDecoration: task.completed ? "line-through" : "none",
+                    }}
+                  >
+                    {task.text}
+                  </span>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleDelete(task.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+            </div>
           </ul>
+
+          {todos.length > 1 && (
+            <div>
+              <button onClick={handleAllDelete}>All Todo Delete</button>
+            </div>
+          )}
+
+          {completedCount() > 0 && (
+            <div>
+              <button>Number of task completed: {completedCount()}</button>
+            </div>
+          )}
         </div>
       </div>
     </div>
